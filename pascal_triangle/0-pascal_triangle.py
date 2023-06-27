232002aed46d33d0e7b2579b662a43f05105dce0
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2023-06-27 09:45:03 +0000

    changes

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -12,6 +12,5 @@ def pascal_triangle(n):
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
-        
         triangle.append(row)
     return triangle
